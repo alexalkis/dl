41530ec9c0edb3cd35ff8b38f87bec9179b278c8
Alex <alkinoos@gmail.com>
2017-07-21 13:35:37 +0300

    add float code in printf, does not link

--- dl/src/pfmt.c
+++ dl/src/pfmt.c
@@ -41,6 +41,12 @@
 #define NOERROR
 
 
+//#define MATH_PFMT   1
+
+#ifdef MATH_PFMT
+#include <math.h>
+#endif
+
 int fwrite(const void *vbuf, size_t elmsize, size_t elms, BPTR fi);
 int _pfmtone(char c, va_list *pva,
 		unsigned int (*func)(char *, size_t, size_t, void *), void *desc,
@@ -213,7 +219,166 @@ int _pfmtone(char c, va_list *pva,
 	}
 		break;
 #ifdef MATH_PFMT
-#include <math/pfmt.c>	    /*	cases for math	*/
+//#include 'mathpfmt.c"	    /*	cases for math	*/
+    case 'e':       /*  [-]d.dddddde[+/-]dd     */
+    case 'E':       /*  [-]d.ddddddE[+/-]dd     */
+    case 'f':       /*  [-]d.dddddd             */
+    case 'g':       /*  if exp < -4 or exp > prec use 'e', else use 'f' */
+    case 'G':       /*  if exp < -4 or exp > prec use 'E', else use 'f' */
+	{
+	    static char prefix[32];
+	    short xp;
+	    short _pfmt_round(char *, short, short);
+	    short gtrunc;
+
+	    if ((c == 'g' || c == 'G') && !(flags & F_HASH))
+		gtrunc = 1;
+	    else
+		gtrunc = 0;
+
+	    if (i2 < 0) 	/*  default precision	    */
+		i2 = 6;
+
+	    if (flags & F_DLONG) {
+		va_arg(va, long double);
+		i = xp = 0;
+	    } else {
+		double d;
+
+		d = va_arg(va, double);
+		if (d < 0.0) {
+		    d = -d;
+		    prefix1 = "-";
+		}
+
+		/*
+		 *  construct prefix buffer by extracting one digit at
+		 *  a time
+		 */
+
+		if (d >= 1.0)
+		    xp = (int)log10(d);
+		else
+		    xp = (int)log10(d) - 1;
+
+		d = d * pow(10.0, (double)-xp) + (DBL_EPSILON * 5);
+
+		for (i = 0; i < DBL_DIG; ++i) {
+		    prefix[i] = (int)d;
+		    d = (d - (double)prefix[i]) * 10.0;
+		}
+	    }
+
+	    /*
+	     *	prefix	- prefix array (might have leading zeros)
+	     *	i	- number of digits in array
+	     *	xp	- exponent of first digit
+	     *
+	     *	handle %g and %G
+	     */
+
+	    if (c == 'g' || c == 'G') {
+		if (xp < -4 || xp >= i2)
+		    c = (c == 'g') ? 'e' : 'E';
+		else
+		    c = 'f';
+	    }
+
+	    /*
+	     *	handle %e and %E
+	     */
+
+	    value = buf + sizeof(buf) - MATH_BUFSIZE;
+
+	    if (c == 'e' || c == 'E') {
+		if ((i = _pfmt_round(prefix, i2 + 1, i)) < 0) {
+		    i = -i;
+		    ++xp;
+		}
+		if (i == 0)
+		    xp = 0;
+		value[0] = prefix[0] + '0';
+		if (i2 > 0 && (gtrunc == 0 || i > 1)) {
+		    value[1] = '.';
+		    for (len = 0; len < i2 && len < (MATH_BUFSIZE - 8); ++len) {
+			if (len + 1 >= i) {
+			    if (gtrunc == 0)
+				trail_zero = i2 - len;
+			    break;
+			}
+			value[len+2] = prefix[len+1] + '0';
+		    }
+
+		    /*
+		     *	put in at least one '0' after the decimal!
+		     *	(don't have to check gtrunc because prefix
+		     *	array has at least 2 entries)
+		     */
+
+		    if (trail_zero == 0 && len == 0) {
+			value[2] = '0';
+			++len;
+		    }
+		    ++len;
+		}
+		++len;
+		{
+		    short yp = (xp < 0) ? -xp : xp;
+
+		    postfix1 = value + len;
+		    postfix1[0] = c;
+		    postfix1[1] = (xp < 0) ? '-' : '+';
+		    postfix1[2] = yp / 100 + '0';
+		    postfix1[3] = yp / 10 % 10 + '0';
+		    postfix1[4] = yp % 10 + '0';
+		    postfix1[5] = 0;
+		}
+	    } else {
+		short pi;
+
+		if ((i = _pfmt_round(prefix, i2 + 1 + xp, i)) < 0) {
+		    i = -i;
+		    ++xp;
+		}
+		for (len = pi = 0; len < MATH_BUFSIZE && xp >= 0; ++len) {
+		    if (pi < i) {
+			value[len] = prefix[pi] + '0';
+			++pi;
+		    } else {
+			value[len] = '0';
+		    }
+		    --xp;
+		}
+		if (len == 0)
+		    value[len++] = '0';
+
+		/* XXX */
+
+		/*
+		 *  pre-zeros
+		 */
+
+		if (len < MATH_BUFSIZE && i2 > 0 && (gtrunc == 0 || pi < i)) {
+		    short prec = 0;
+
+		    value[len++] = '.';
+
+		    while (len < MATH_BUFSIZE && -(prec + 1) > xp && prec < i2) {
+			value[len++] = '0';
+			++prec;
+		    }
+		    while (len < MATH_BUFSIZE && prec < i2) {
+			if (pi < i)
+			    value[len++] = prefix[pi++] + '0';
+			else if (gtrunc == 0)
+			    value[len++] = '0';
+			++prec;
+		    }
+		}
+	    }
+	}
+	break;
+
 #else
 	case 'e': /*  [-]d.dddddde[+/-]dd     */
 	case 'E': /*  [-]d.ddddddE[+/-]dd     */
@@ -400,7 +565,44 @@ int _pfmtone(char c, va_list *pva,
 }
 
 #ifdef MATH_PFMT
-#include <math/pfmt.c>	    /*	aux routines for math	*/
+//#include "mathpfmt.c"	    /*	aux routines for math	*/
+short int  _pfmt_round(char *prefix, short int ri, short int max)
+{
+    short carry;
+    char *ptr;
+
+    if (ri < max) {	/*  handle rounding + the log boundry case	    */
+	max = ri;
+	if (max < 0)
+	    max = 0;
+	carry = 5;
+    } else {		/*  handle log boundry case (entries that are 10)   */
+	if (max)
+	    ri = --max;
+	else
+	    ri = -1;
+	carry = 0;
+    }
+    for (ptr = prefix + ri; ri >= 0; --ri, --ptr) {
+	*ptr += carry;
+	if (*ptr >= 10) {
+	    *ptr -= 10;
+	    carry = 1;
+	} else {
+	    carry = 0;
+	}
+	if (*ptr == 0 && ri == max - 1)
+	    --max;
+    }
+    if (carry) {
+	movmem(prefix, prefix + 1, max);
+	++max;
+	prefix[0] = 1;
+	return(-max);
+    }
+    return(max);
+}
+
 #endif
 
 /*
